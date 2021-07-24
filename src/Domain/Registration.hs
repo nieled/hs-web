5f26dc9c89418d1e153454a7e545f0f8968a813f
Daniel Calle <nieled@riseup.net>
2021-07-24 10:03:29 -0500

    Implement register with IO

--- src/Domain/Registration.hs
+++ src/Domain/Registration.hs
@@ -1,7 +1,10 @@
-module Domain.Registration where
+module Domain.Registration
+  ( register
+  ) where
 
 import ClassyPrelude
 import Domain.Auth
+import Control.Monad.Except
 
 type VerificationCode = Text
 
@@ -10,4 +13,23 @@ class Monad m => AuthRepo m where
 
 class Monad m => EmailVerificationNotif m where
   notifyEmailVerification :: Email -> VerificationCode -> m ()
+
+instance AuthRepo IO where
+  addAuth (Auth email pass) = do
+    putStrLn $ "adding auth: " <> rawEmail email
+    return $ Right "fake verification code"
+
+instance EmailVerificationNotif IO where
+  notifyEmailVerification email vCode =
+    putStrLn $ "Notify " <> rawEmail email <> " - " <> vCode
+
+register
+  :: (AuthRepo m, EmailVerificationNotif m)
+  => Auth
+  -> m (Either RegistrationError ())
+register auth =
+  runExceptT $ do
+    vCode <- ExceptT $ addAuth auth
+    let email = authEmail auth
+    lift $ notifyEmailVerification email vCode
 

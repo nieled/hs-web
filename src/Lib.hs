6e955209081f63fe1463d70471266234bf979708
Daniel Calle <nieled@riseup.net>
2021-07-25 20:44:30 -0500

    Katip sample code

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,13 @@
 module Lib
     ( someFunc
+    , runKatip
     ) where
 
 import qualified Adapter.InMemory.Auth as M
 import           ClassyPrelude
 import           Control.Monad
 import           Domain.Auth
+import           Katip
 
 someFunc :: IO ()
 someFunc = do
@@ -48,3 +50,28 @@ instance SessionRepo App where
 
 instance MonadFail App where
   fail = undefined
+
+runKatip :: IO ()
+runKatip = withKatip $ \le ->
+  runKatipContextT le () mempty logSomething
+
+withKatip :: (LogEnv -> IO a) -> IO a
+withKatip = bracket createLogEnv closeScribes
+  where
+    createLogEnv = do
+      logEnv <- initLogEnv "HAuth" "dev"
+      stdoutScribe <- mkHandleScribe ColorIfTerminal stdout (permitItem InfoS) V2
+      registerScribe "stdout" stdoutScribe defaultScribeSettings logEnv
+
+logSomething :: (KatipContext m) => m ()
+logSomething = do
+  $(logTM) InfoS "Log in no namespace"
+  katipAddNamespace "ns1" $
+    $(logTM) InfoS "Log in ns1"
+  katipAddNamespace "ns2" $ do
+    $(logTM) WarningS "Log in ns2"
+    katipAddNamespace "ns3" $
+      katipAddContext (sl "userId" $ asText "12") $ do
+        $(logTM) InfoS "Log in ns2.ns3 with userId context"
+        katipAddContext (sl "country" $ asText "Singapore") $
+          $(logTM) InfoS "Log in ns2.ns3 with userId and country context"
